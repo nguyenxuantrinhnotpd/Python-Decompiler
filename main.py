17114d673ca380acd8b5ba83e7afc4fc9ccfb6d7
nguyenxuantrinhnotpd <142568632+nguyenxuantrinhnotpd@users.noreply.github.com>
2025-07-08 08:36:25 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+"""
+@Author: Nguyễn Xuân Trịnh 
+@Telegram: CalceIsMe
+@Note:
+THIS IS A SIMPLE DECOMPILER THAT SUPPORTS SIMPLE PYTHON OPCODES.
+IF YOU WANT MORE, THEN CODE TO SUPPORT OTHER OPCODES.
+"""
+import dis, ast
+class DECOMPILER:
+    def __init__(self, bytecode):
+        self.bytecode = bytecode
+        self.stack = []
+        self.source_code = []
+        self.in_function = False
+    def decompile(self):
+        opcodes = []
+        for instr in dis.get_instructions(self.bytecode):
+            opcodes.append(instr.opname)
+            if instr.opname == 'RESUME':
+                pass
+            elif instr.opname == 'PUSH_NULL':
+                self.stack.append(None)
+            elif instr.opname == 'LOAD_CONST':
+                self.stack.append(instr.argrepr)
+            elif instr.opname == 'LOAD_NAME':
+                self.stack.append(instr.argval)
+            elif instr.opname == 'CALL':
+                args = []
+                for _ in range(instr.arg):
+                    args.append(self.stack.pop())
+                args = args[::-1]
+                func = self.stack.pop()
+                self.source_code.append(f"{func}({', '.join(args)})")
+            elif instr.opname == 'POP_TOP':
+                if self.stack:
+                    self.stack.pop()
+            elif instr.opname == 'RETURN_CONST':
+                v = instr.argval
+                if self.in_function:
+                    if v is None:
+                        self.source_code.append("return")
+                    else:
+                        self.source_code.append(f"return {repr(v)}")
+            else:
+                self.source_code.append(f" Unsupported Opcode {instr.opname}")
+        for opcode in opcodes:
+            print(opcode)
+        print("= "*10)
+        self.source_code = '\n'.join(self.source_code)
+        tree = ast.parse(self.source_code)
+        ast.fix_missing_locations(tree)
+        self.source_code = (ast.unparse(tree))
+        print(self.source_code)
+        print("= "*10)
+        print("Stack Remaning:",self.stack)
+code = """
+print("Hello World!")
+print(a,b)
+"""
+bytecode = compile(code,"<string>","exec")
+dis.dis(bytecode)
+decompile = DECOMPILER(bytecode).decompile()
